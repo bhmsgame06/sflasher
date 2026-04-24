9febb60d65316c81925e1c4f0c926b9da1ca3f89
bhmsgame06 <bhmsgamexbox2010@gmail.com>
2026-04-24 16:42:42 +0500

    extern

--- preloader/src/flash.h
+++ preloader/src/flash.h
@@ -13,43 +13,43 @@ typedef uint32_t FLASH_BLK_OFFSET;
 
 /* Autoselect */
 
-uint16_t flash_manufacturer_id(void);
+extern uint16_t flash_manufacturer_id(void);
 
-uint16_t flash_device_id(void);
+extern uint16_t flash_device_id(void);
 
-uint16_t flash_blk_prot_verify(FLASH_BLK blk);
+extern uint16_t flash_blk_prot_verify(FLASH_BLK blk);
 
-uint16_t flash_handshaking(void);
+extern uint16_t flash_handshaking(void);
 
 /* Flash operations */
 
-void flash_program(FLASH_BLK blk, FLASH_BLK_OFFSET off, uint16_t value);
+extern void flash_program(FLASH_BLK blk, FLASH_BLK_OFFSET off, uint16_t value);
 
-void flash_blk_erase(FLASH_BLK blk);
+extern void flash_blk_erase(FLASH_BLK blk);
 
-void flash_chip_erase(void);
+extern void flash_chip_erase(void);
 
-void flash_blk_protect(FLASH_BLK blk);
+extern void flash_blk_protect(FLASH_BLK blk);
 
-void flash_blk_unprotect(FLASH_BLK blk);
+extern void flash_blk_unprotect(FLASH_BLK blk);
 
-void flash_buffer_write(FLASH_BLK blk, FLASH_BLK_OFFSET off, void *buf, uint32_t wc);
+extern void flash_buffer_write(FLASH_BLK blk, FLASH_BLK_OFFSET off, void *buf, uint32_t wc);
 
-void flash_buffer_program(FLASH_BLK blk);
+extern void flash_buffer_program(FLASH_BLK blk);
 
-void flash_buffer_abort(void);
+extern void flash_buffer_abort(void);
 
 /* Unlock Bypass operations */
 
-void flash_unlock_bypass(void);
+extern void flash_unlock_bypass(void);
 
-void flash_unlock_bypass_reset(void);
+extern void flash_unlock_bypass_reset(void);
 
-void flash_unlock_bypass_program(FLASH_BLK blk, FLASH_BLK_OFFSET off, uint16_t value);
+extern void flash_unlock_bypass_program(FLASH_BLK blk, FLASH_BLK_OFFSET off, uint16_t value);
 
-void flash_unlock_bypass_blk_erase(FLASH_BLK blk);
+extern void flash_unlock_bypass_blk_erase(FLASH_BLK blk);
 
-void flash_unlock_bypass_chip_erase(void);
+extern void flash_unlock_bypass_chip_erase(void);
 
 #endif /* _FLASH_H */
 
